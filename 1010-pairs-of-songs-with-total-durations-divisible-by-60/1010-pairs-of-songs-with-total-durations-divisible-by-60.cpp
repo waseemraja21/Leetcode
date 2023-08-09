95a999ec1ff051c65f3561e05afa20b825dc8ec5
waseemraja21 <114870429+waseemraja21@users.noreply.github.com>
2023-08-09 23:36:21 +0530

    Time: 39 ms (72.72%), Space: 28.3 MB (6.29%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     int numPairsDivisibleBy60(vector<int>& time) {
         int count = 0;
-        vector<int> arr(60, 0);
+        unordered_map<int, int> mp;
         
         for(int i=0; i<time.size(); i++){
+            // dont do anything with count when rem is 0 and adding with those no of elements that when added with current will produce a pair like 40 and 20 , 100 and 20
             int rem = time[i]%60;
             if(rem == 0){
-                count += arr[0]; // dont do anything with count
-            }else{
-                count += arr[60-rem];   // adding with those no of elements that when added with current will produce a pair like 40 and 20 , 100 and 20
+                count += mp[0];
             }
-            arr[rem]++;
+            else{
+                 count += mp[60-rem];
+            }
+             
+            
+            mp[rem]++;
         }
         return count;
         
