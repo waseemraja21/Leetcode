200547e5a8585070f73de125c0daab4c9741b058
waseemraja21 <114870429+waseemraja21@users.noreply.github.com>
2023-09-09 09:25:13 +0530

    Added solution - LeetHub

---
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    /*  Function to implement Bellman Ford
+    *   edges: vector of vectors which represents the graph
+    *   S: source vertex to start traversing graph with
+    *   V: number of vertices
+    */
+    vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
+        // Code here
+        
+        vector<int> dist(V, 1e8);
+        dist[S] = 0;
+        
+        // realxing edges for v-1 times
+        // in n-1 iteration it would have find all minimum distances
+        
+        for(int i=0; i<V; i++){     // iterations need to made for N-1 times to relax edges bxz order of edges can be anything 
+            for(auto it : edges){
+                int u = it[0];
+                int v = it[1];
+                int wt = it[2];
+                
+                if(dist[u] != 1e8 && dist[u] + wt < dist[v]){
+                    dist[v] = dist[u]+wt;
+                }
+            }
+        }
+        
+        // checking for negative cycle
+        // checking nth time for negative cycle bxz if at nth time if edges are still relaxing it means there is negative cycle
+        for(auto it : edges){
+                int u = it[0];
+                int v = it[1];
+                int wt = it[2];
+                
+                if(dist[u] != 1e8 && dist[u] + wt < dist[v]){
+                    return {-1};
+                }
+        }
+        
+        return dist;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, m;
+        cin >> N >> m;
+        vector<vector<int>> edges;
+
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 3; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+
+        int src;
+        cin >> src;
+
+        Solution obj;
+        vector<int> res = obj.bellman_ford(N, edges, src);
+
+        for (auto x : res) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
