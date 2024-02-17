91271de7b602d6e9d24e0e742ddba796e59c9a20
waseemraja21 <114870429+waseemraja21@users.noreply.github.com>
2024-02-17 09:58:59 +0530

    Time: 12 ms (58.08%), Space: 16.5 MB (23.33%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,35 +1,35 @@
 class Solution {
     
-    int findProfit(int day, int k, int sell, int n, vector<vector<vector<int>>> &dp, vector<int>& prices){
-        
-        if(day == n || k == 0) return 0;
-        
-        if(dp[day][sell][k] != -1) return dp[day][sell][k];
-        
-        int profit = 0;
-        if(sell == 0){
-            int op1 = 0+findProfit(day+1, k, 0, n, dp, prices);
-            int op2 = -prices[day]+findProfit(day+1, k, 1, n, dp, prices);
-            profit = max(op1, op2);
-        }
-        
-        if(sell == 1){
-            int op1 = 0+findProfit(day+1, k, 1, n, dp, prices);
-            int op2 = prices[day]+findProfit(day+1, k-1, 0, n, dp, prices);
-            profit = max(op1, op2);
-        }
-        
-        return dp[day][sell][k] = profit;
-    }
-    
 public:
     int maxProfit(int k, vector<int>& prices) {
         
         int n= prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k+1,-1)));
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1,0)));
         
-        int sell = 0;
+        int profit = 0;
         
-        return findProfit(0, k, sell, n, dp, prices);
+        for(int day = n-1; day >= 0; day--){
+            for(int sell = 0; sell <= 1; sell++){
+                for(int tran = 1; tran <= k; tran++){
+                    if(sell == 0){
+                        int op1 = 0+dp[day+1][0][tran];
+                        int op2 = -prices[day]+dp[day+1][1][tran];
+                        profit = max(op1, op2);
+                    }
+                    if(sell == 1){
+                        int op1 = 0+dp[day+1][1][tran];
+                        int op2 = prices[day]+dp[day+1][0][tran-1];
+                        profit = max(op1, op2);
+                    }
+                    
+                    dp[day][sell][tran] = profit;
+                }
+            }
+        }
+        
+        return dp[0][0][k];
+        
+        
+        
     }
 };
